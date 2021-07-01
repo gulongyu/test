0794e4fa178f548ccb55712b165630a45ca3ff7f
gulongyu <2108204334@qq.com>
2021-07-01 11:16:19 +0800

    增加es搜索功能

--- src/main/java/com/example/demo/controller/EsController.java
+++ src/main/java/com/example/demo/controller/EsController.java
@@ -6,7 +6,7 @@ package com.example.demo.controller;
  * @Date:2021/6/30
  */
 public class EsController {
-
+    private int sum = 0;
     public static void main(String[] args) {
         System.out.println("测试");
     }
